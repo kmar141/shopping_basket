a2c51e4b9a8260b7a8394cae26d9da5ae6239336
Kieran Marshall <youthassassin@gmail.com>
2017-07-26 18:33:23 +0100

    zzzzzzz

--- app/src/test/java/com/example/kieran/shoppingbasket/TestCheckout.java
+++ app/src/test/java/com/example/kieran/shoppingbasket/TestCheckout.java
@@ -1,8 +1,30 @@
 package com.example.kieran.shoppingbasket;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Kieran on 26/07/2017.
  */
 
 public class TestCheckout {
+
+    Checkout checkout;
+    Basket items;
+    Item socks;
+    Item book;
+    Item umbrella;
+    Customer kieran;
+
+    @Before
+    public void setUp() throws Exception {
+        checkout = new Checkout();
+        items = new Basket();
+        socks = new Item(2);
+        book = new Item(4);
+        umbrella = new Item(8);
+        kieran = new Customer();
+
+    }
+
 }
